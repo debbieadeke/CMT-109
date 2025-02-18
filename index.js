232f3edf8bf74338a332b9bdb158b2bc1679bbdb
debbieadeke <okakadebbie15@gmail.com>
2025-02-18 20:48:41 +0300

    ✨ Add js on navbar

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const navLinks = document.querySelectorAll("nav ul li a");
+    const navLinks = document.querySelectorAll(".navbar a");
 
+    
     navLinks.forEach(link => {
-        link.addEventListener("click", function (e) {
-            e.preventDefault();
-            const targetId = this.getAttribute("href").substring(1);
-            const targetElement = document.getElementById(targetId);
-            if (targetElement) {
-                window.scrollTo({
-                    top: targetElement.offsetTop - 50,
-                    behavior: "smooth"
-                });
-            }
-        });
+        if (window.location.href.includes(link.href)) {
+            link.classList.add("active");
+        }
     });
 });
+
